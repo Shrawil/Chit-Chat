dfce11f3219731a8c8a6cc0986598bcbaef603c7
Shrawil <shrawilsrivastavaa@gmail.com>
2025-07-07 11:20:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,20 +1,46 @@
+const allowedUsers = {
+  "Shrawil": "shrawilsri8381",
+  "Akriti": "akritithedon",
+  "Tiya": "tiyaistia",
+  "Amardeep Singh": "baapji",
+  "Seema Srivastava": "seema102",
+  "Indra": "sigmaindra",
+  "Diva": "1210diva",
+  "Adarsh": "5footdon",
+  "Raj": "dacsahab",
+};
+
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+const bodyParser = require('body-parser');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// Serve files from "public" folder
+// Middleware
+app.use(bodyParser.json());
+
+// Login route
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  if (allowedUsers[username] === password) {
+    res.json({ success: true });
+  } else {
+    res.status(401).json({ success: false, message: "Invalid credentials" });
+  }
+});
+
+// Serve public folder
 app.use(express.static('public'));
 
-// Socket connection
+// Socket.io logic
 io.on('connection', (socket) => {
   console.log('A user connected');
 
-  socket.on('chat message', (msgObj) => {
-    io.emit('chat message', msgObj);
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
   });
 
   socket.on('disconnect', () => {
@@ -22,9 +48,7 @@ io.on('connection', (socket) => {
   });
 });
 
-// Start the server
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
